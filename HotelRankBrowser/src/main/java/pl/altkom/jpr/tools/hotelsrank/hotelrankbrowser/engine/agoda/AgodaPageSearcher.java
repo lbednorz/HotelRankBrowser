f7dda7976f5bc5bd17e310a93cbc8f05994c0fe2
KOMPAXpiotrpawlus <biuro@kompax.it>
2014-05-11 19:01:18 +0200

    Update AgodaPageSearcher.java

--- HotelRankBrowser/src/main/java/pl/altkom/jpr/tools/hotelsrank/hotelrankbrowser/engine/agoda/AgodaPageSearcher.java
+++ HotelRankBrowser/src/main/java/pl/altkom/jpr/tools/hotelsrank/hotelrankbrowser/engine/agoda/AgodaPageSearcher.java
@@ -14,39 +14,56 @@ import pl.altkom.jpr.tools.hotelsrank.hotelrankbrowser.engine.HotelPageSearcher;
 public final class AgodaPageSearcher implements HotelPageSearcher {
 
 	private static final String COUNTRY_XML = "country.xml";
+	private static final String COUNTRY_ATTRIB = "alpha-2";
+	private static final String COUNTRY_PATTERN = "/countries/country[@name='%s']";
 	private static final String URL = "http://www.agoda.com/pl-pl/%s/hotel/%s-%s.html";
 
-
 	public String searchPage(String hotelName, String city, String country) {
-		String url = String.format(URL, hotelName, city,
-				prepareCountry(country));
+		String url = String.format(URL, prepareHotelName(hotelName),
+				prepareCity(city), prepareCountry(country));
 		return url;
 	}
 
+	private String prepareHotelName(String hotelName) {
+		hotelName = hotelName.replaceAll(" ", "-");
+		return hotelName.toLowerCase();
+	}
+
+	private String prepareCity(String city) {
+		return city.toLowerCase();
+	}
+
 	private String prepareExpression(String country) {
-		StringBuilder sb = new StringBuilder();
-		sb.append("/countries/country[@name='");
-		sb.append(country);
-		sb.append("']");
-		return sb.toString();
+		return String.format(COUNTRY_PATTERN, country);
+	}
+
+	private Document prepareDocumentCountry() throws Exception {
+		InputStream resourceAsStream = Test.class
+				.getResourceAsStream(COUNTRY_XML);
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(resourceAsStream);
+		return doc;
+	}
+
+	private String getAttribCountry(Document doc, String country)
+			throws Exception {
+		XPath xPath = XPathFactory.newInstance().newXPath();
+		String expression = prepareExpression(country);
+		Element evaluate = (Element) xPath.compile(expression).evaluate(doc,
+				XPathConstants.NODE);
+		String result = evaluate.getAttribute(COUNTRY_ATTRIB);
+		return result;
 	}
 
 	private String prepareCountry(String country) {
 		try {
-			InputStream resourceAsStream = AgodaPageSearcher.class
-					.getResourceAsStream(COUNTRY_XML);
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
-					.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(resourceAsStream);
-			XPath xPath = XPathFactory.newInstance().newXPath();
-			String expression = prepareExpression(country);
-			String result = xPath.compile(expression).evaluate(doc);
-			return result;
+			Document doc = prepareDocumentCountry();
+			String result = getAttribCountry(doc, country);
+			return result.toLowerCase();
 		} catch (Exception e) {
-			return null;
+			return "";
 		}
-
 	}
 
 }
